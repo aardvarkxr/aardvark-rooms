1f533d590c23151232d833229444be506aecc487
Joe Ludwig <joe@valvesoftware.com>
2020-10-18 10:32:41 -0700

    test fixes
    * Fixed exports from room-shared
    * fixed module paths for jest
    * Added RoomTestClient that does some of the things that were hand-rolled in the test cases.

--- packages/room-server/src/__tests__/room_server_tests.ts
+++ packages/room-server/src/__tests__/room_server_tests.ts
@@ -1,5 +1,7 @@
+import { RoomMessage } from '@aardvarkxr/room-shared';
 import { RoomServer } from './../room_server_main';
 import { MessageType, AardvarkPort, MsgSetEndpointType, EndpointType, Envelope, MsgSetEndpointTypeResponse, MsgGetAardvarkManifest, MsgGeAardvarkManifestResponse } from '@aardvarkxr/aardvark-shared';
+import bind from 'bind-decorator';
 import WebSocket = require('ws' );
 
 
@@ -21,25 +23,82 @@ afterEach( async () =>
 } );
 
 
+class RoomTestClient
+{
+	public ws = new WebSocket( `ws://localhost:${ server?.portNumber }` );
+	public connected = false;
+	public connectResolves: ( ( res: boolean )=>void )[] = [];
+
+	public messages: RoomMessage[] = [];
+
+	constructor( )
+	{
+
+		this.ws.onopen = this.onConnect;
+		this.ws.onmessage = this.onMessage;
+		this.ws.onerror = this.onError;
+	}
+
+	@bind
+	public onMessage( event: WebSocket.MessageEvent )
+	{
+		let msg = JSON.parse( event.data as string ) as RoomMessage;
+		this.messages.push( msg );
+	}
+
+	@bind
+	private onConnect( )
+	{
+		this.connected = true;
+
+		for( let resolve of this.connectResolves )
+		{
+			resolve( true );
+		}
+		this.connectResolves = [];
+	}
+
+	@bind
+	private onError( evt: WebSocket.ErrorEvent )
+	{
+		if( !this.connected )
+		{
+			for( let resolve of this.connectResolves )
+			{
+				resolve( false );
+			}
+			this.connectResolves = [];
+		}
+	}
+	
+	public waitForConnect() : Promise<boolean>
+	{
+		return new Promise<boolean>( (resolve, reject) =>
+		{
+			if( this.connected )
+			{
+				resolve( true );
+			}
+
+			this.connectResolves.push( resolve );
+		} );
+	}
+
+	public close()
+	{
+		this.ws?.close();
+	}
+}
+
+
 describe( "RoomServer ", () =>
 {
 	it( "connect", async ( done ) =>
 	{
- 		let addr = `ws://localhost:${ server?.portNumber }`;
+		let client = new RoomTestClient();
+		let res = await client.waitForConnect();
 
-		let client = new WebSocket( addr );
-		client.on( 'error', (sock:WebSocket, code: number, reason: string ) =>
-		{
-			throw new Error( reason );
-		} );
-
-		let p = new Promise<void>( ( resolve, reject ) =>
-		{
-			client.onopen = (evt: WebSocket.OpenEvent ) => { resolve() } ;
-		} );
-
-		await p;
-
+		expect( res ).toBe( true );
 		client.close();
 		done();
 	} );
