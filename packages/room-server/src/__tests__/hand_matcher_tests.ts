71f3fac6c88963595be89cb0e91d259db1d4931c
Joe Ludwig <joe@valvesoftware.com>
2020-11-03 09:02:35 -0800

    More matcher testing
    Added helper to expect for context lists
    Added simple match test case.

--- packages/room-server/src/__tests__/hand_matcher_tests.ts
+++ packages/room-server/src/__tests__/hand_matcher_tests.ts
@@ -1,12 +1,57 @@
 import { HandMatcher, HandSample, MatchResult } from './../hand_matcher';
-import { RoomMessage, RoomMessageType, RoomResult } from '@aardvarkxr/room-shared';
-import { RoomServer } from '../room_server';
-import bind from 'bind-decorator';
-import WebSocket = require('ws' );
 
 
 jest.useRealTimers();
 
+
+expect.extend( 
+{
+	toHaveContexts( actualContexts: any[], expectedContexts: any[] )
+	{
+		let foundCount = 0;
+		for( let context of expectedContexts )
+		{
+			if( actualContexts.includes( context ) )
+			{
+				foundCount++;
+			}
+		}
+
+		let pass: boolean;
+		let reason: string;
+		if( actualContexts.length != expectedContexts.length )
+		{
+			pass = false;
+			reason = `Actual (${ actualContexts.length }) and expected (${ expectedContexts.length }) lengths don't match`;
+		}
+		else if( foundCount == expectedContexts.length )
+		{
+			pass = true;
+			reason = `Found (${ foundCount }) of (${ expectedContexts.length }) contexts`;
+		}
+		else
+		{
+			pass = false;
+			reason = `Only found (${ foundCount }) of (${ expectedContexts.length }) contexts`;
+		}
+
+		return {
+			message: () => reason,
+			pass,
+		};
+	}
+} );
+
+declare global 
+{
+	namespace jest 
+	{
+		interface Matchers<R> 
+		{
+			toHaveContexts( context: any[] ): R;
+		}
+	}
+}
 
 
 interface Call
@@ -56,9 +101,21 @@ describe( "RoomServer ", () =>
 		matcher.addSample( s2 );
 		expect( calls.length ).toBe( 1 );
 		expect( calls[0].result ).toBe( MatchResult.Replaced );
-		expect( calls[0].contexts.length ).toBe( 1 );
-		expect( calls[0].contexts[0] ).toBe( s1.context );
+		expect( calls[0].contexts ).toHaveContexts( [ s1.context ] );
+	} );
 
+	it( "simple match", () =>
+	{
+		let s1 = sample( 3, 4, 2 );
+		let s2 = sample( 4, 3, 2 );
+		expect( s2.context ).not.toBe( s1.context );
+
+		matcher.addSample( s1 );
+		expect( calls.length ).toBe( 0 );
+		matcher.addSample( s2 );
+		expect( calls.length ).toBe( 1 );
+		expect( calls[0].result ).toBe( MatchResult.Matched );
+		expect( calls[0].contexts ).toHaveContexts( [ s1.context, s2.context ] );
 	} );
 
 
