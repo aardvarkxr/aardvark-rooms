f1601c4b07acdadb18fcdc6bd522ac595c403e75
Joe Ludwig <joe@valvesoftware.com>
2020-11-15 11:40:34 -0800

    More tweaking
    Heroku will run the build script before start, and times out if we try to build again.
    
    Also port was failing if it wasn't set. Have it default to 18080 if port isn't set

--- packages/room-server/src/room_server_main.ts
+++ packages/room-server/src/room_server_main.ts
@@ -1,6 +1,6 @@
 import { RoomServer } from './room_server';
 
 
-let port = process.argv.length > 2 ? parseInt( process.argv[2] ) : parseInt( process.env.PORT );
+let port = process.argv.length > 2 ? parseInt( process.argv[2] ) : parseInt( process.env.PORT ?? "18080" );
 let server = new RoomServer( port );
 server.init();
